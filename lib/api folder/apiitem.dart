e433fce1ae891e9392e68b192338cd7f359fc48c
msnsadq <nannatsadik@gmail.com>
2022-04-05 14:42:26 +0530

    almost finished

--- lib/api folder/apiitem.dart
+++ lib/api folder/apiitem.dart
@@ -7,11 +7,11 @@ import 'ModelApi.dart';
   Future<ModelApi>fetchApi()async{
   final result = await http.get(Uri.parse('https://api.themoviedb.org/3/trending/all/day?api_key=a5621ef03b6cb42a8c1ffb828d33ba8c'));
 
-    print(result.body);
+   // print(result.body);
     final values = json.decode(result.body);
      ModelApi.fromJson(values);
     return ModelApi.fromJson(values);
-
+    
 
 
 
--- lib/home.dart
+++ lib/home.dart
@@ -10,29 +10,76 @@ class ScreenHome extends StatefulWidget {
 }
 
 class _ScreenHomeState extends State<ScreenHome> {
-
+  final String baseurl = "https://image.tmdb.org/t/p/w185";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchApi();
+   // fetchApi();
   }
-  @override
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-          future: fetchApi(), builder: (BuildContext context, AsyncSnapshot<ModelApi> snapshot) {
-
-          if(snapshot.hasData){
-            return Center(
-              child: Text(snapshot.data!.results![0].voteCount.toString()),
-            );
-          }
-          return Container();
-        }
-
-         )
-    );
+        body: FutureBuilder(
+            future: fetchApi(),
+            builder: (BuildContext context, AsyncSnapshot<ModelApi> snapshot) {
+              if (snapshot.hasData) {
+                // return Center(
+                //   child: Container(
+                //     height: 200,
+                //     width: double.infinity,
+                //     child: Image.network(
+                //       "https://image.tmdb.org/t/p/w185" +
+                //           snapshot.data!.results![1].backdropPath.toString(),
+                //       fit: BoxFit.cover,
+                //     ),
+                //   ),
+                // );
+                return Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage("assets/image/bgpic.jpg",),fit: BoxFit.cover)
+                  ),
+                  child: Padding(
+                    padding:  EdgeInsets.only(top:8.0, bottom: 8,left: 8,right: 8),
+                    child: GridView.count(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 28,
+                        crossAxisSpacing: 0,
+                        children:
+                            List.generate(snapshot.data!.results!.length, (index) {
+                          return Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                                child: Card(
+                                  elevation: 5,
+                                  color: Colors.grey,
+                                 // margin: EdgeInsets.symmetric(horizontal: 33),
+                                  child: Image.network(
+                                    baseurl +
+                                        snapshot.data!.results![index].posterPath
+                                            .toString(),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text(
+                                    snapshot.data!.results![index].title.toString(),
+                                    style: TextStyle(color: Colors.white,),
+                                  ))
+                            ],
+                          );
+                        })),
+                  ),
+                );
+              }
+              return Container();
+            }));
   }
 }
