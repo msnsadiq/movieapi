1a0c28d77abfd663a627acc6fabb13a6b860921c
msnsadq <nannatsadik@gmail.com>
2022-04-08 14:51:43 +0530

    only few editings left

--- lib/home.dart
+++ lib/home.dart
@@ -1,111 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:statemanagement/api%20folder/ModelApi.dart';
 import 'package:statemanagement/api%20folder/apiitem.dart';
 import 'package:statemanagement/moonfall%20api/MoonfallModel.dart';
 import 'package:statemanagement/moonfall%20api/moonfallapii.dart';
 import 'package:statemanagement/secondpage.dart';
 
-class ScreenHome extends StatefulWidget {
-  const ScreenHome({Key? key}) : super(key: key);
+class ScreenHome extends StatelessWidget {
 
-  @override
-  State<ScreenHome> createState() => _ScreenHomeState();
-}
-
-class _ScreenHomeState extends State<ScreenHome> {
   final String baseurl = "https://image.tmdb.org/t/p/w185";
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-   // fetchApi();
-  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 20,
+        title: Text("Trending Movies",style: GoogleFonts.aclonica(),),
+
+        backgroundColor: Colors.grey,
+      ),
         body: FutureBuilder(
             future: fetchApi(),
             builder: (BuildContext context, AsyncSnapshot<ModelApi> snapshot) {
               if (snapshot.hasData) {
-                // return Center(
-                //   child: Container(
-                //     height: 200,
-                //     width: double.infinity,
-                //     child: Image.network(
-                //       "https://image.tmdb.org/t/p/w185" +
-                //           snapshot.data!.results![1].backdropPath.toString(),
-                //       fit: BoxFit.cover,
-                //     ),
-                //   ),
-                // );
+
                 return Container(
                   decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage("assets/image/bgpic.jpg",),fit: BoxFit.cover)
-                  ),
+                      image: DecorationImage(
+                          image: AssetImage(
+                            "assets/image/bgpic.jpg",
+                          ),
+                          fit: BoxFit.cover)),
                   child: Padding(
-                    padding:  EdgeInsets.only(top:8.0, bottom: 8,left: 8,right: 8),
-                    child: GridView.count(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 20,
-                        crossAxisSpacing: 2,
-                        children:
-                            List.generate(snapshot.data!.results!.length, (index) {
-
-                          return Stack(
-
-                            children: [
-
-                              Positioned(
-                                left: 0,
-                                right: 0,
-                                top: 0,
-                                bottom: 0,
-                                child: GestureDetector(
-                                  onTap: (){
-                                    print("taped $index");
-                                    Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=>
-                                        HomeTwo(name: snapshot.data!.results![index].id.toString(),
-                                          image:baseurl + snapshot.data!.results![index].posterPath.toString(),)));
-                                  },
-                                  child: Card(
-                                    elevation: 15,
-                                    shadowColor: Colors.black,
-                                    color: Colors.grey,
-                                   // margin: EdgeInsets.symmetric(horizontal: 33),
-                                    child: Image.network(
-                                      baseurl +
-                                          snapshot.data!.results![index].posterPath
-                                              .toString(),
-                                      fit: BoxFit.cover,
-                                    ),
+                    padding:
+                        EdgeInsets.only(top: 3.0, bottom: 8, left: 8, right: 8),
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2
+                      ),
+                        itemCount:snapshot.data!.results!.length,
+                        itemBuilder: (ctx, index){
+                        return Stack(
+                          children: [
+                            Positioned(
+                              left: 0,
+                              right: 0,
+                              top: 0,
+                              bottom: 0,
+                              child: GestureDetector(
+                                onTap: () {
+                                  print("taped $index");
+                                  Navigator.of(context)
+                                      .push(MaterialPageRoute(
+                                      builder: (ctx) => HomeTwo(
+                                        iddata: snapshot
+                                            .data!.results![index].id
+                                            .toString(),
+                                        // image: baseurl +
+                                        //     snapshot
+                                        //         .data!
+                                        //         .results![index]
+                                        //         .posterPath
+                                        //         .toString(),
+                                      )));
+                                },
+                                child: Card(
+                                  elevation: 15,
+                                  shadowColor: Colors.black,
+                                  color: Colors.grey,
+                                  // margin: EdgeInsets.symmetric(horizontal: 33),
+                                  child: Image.network(
+                                    baseurl +
+                                        snapshot
+                                            .data!.results![index].posterPath
+                                            .toString(),
+                                    fit: BoxFit.fill,
                                   ),
                                 ),
                               ),
-                              snapshot.data!.results![index].title != null ? Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Text(
-                                  snapshot.data!.results![index].title.toString(),
-                                  style: TextStyle(color: Colors.white,fontSize: 11,
-                                      fontFamily: "titlestyle",
-                                  ),
-                                ),
-                              ): Container(),
-                              // FutureBuilder(
-                              //   // future: moonfall(),
-                              //   builder: (BuildContext context, AsyncSnapshot<MoonfallModel> snapshot) {
-                              //   if(snapshot.hasData){
-                              //     return Column(
-                              //
-                              //     );
-                              //
-                              //   }
-                              //   return Container();
-                              // },),
-                             // HomeTwo(name: "fasil $index")
-                            ],
-                          );
-                        })),
+                            ),
+                            snapshot.data!.results![index].title != null
+                                ? Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                  snapshot.data!.results![index].title
+                                      .toString(),
+                                  style: GoogleFonts.michroma(
+                                    color: Colors.white,
+                                    fontSize: 11,
+
+                                  )
+                              ),
+                            )
+                                : Container(),
+
+                            // FutureBuilder(
+                            //   // future: moonfall(),
+                            //   builder: (BuildContext context, AsyncSnapshot<MoonfallModel> snapshot) {
+                            //   if(snapshot.hasData){
+                            //     return Column(
+                            //
+                            //     );
+                            //
+                            //   }
+                            //   return Container();
+                            // },),
+                            // HomeTwo(name: "fasil $index")
+                          ],
+                        );
+                        },
+                        // crossAxisCount: 2,
+                        // mainAxisSpacing: 20,
+                        // crossAxisSpacing: 2,
+
+                    ),
                   ),
                 );
               }
--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,47 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:statemanagement/moonfall%20api/MoonfallModel.dart';
 import 'package:statemanagement/moonfall%20api/moonfallapii.dart';
 
-class HomeTwo extends StatefulWidget {
-  HomeTwo({Key? key,required this.name, required this.image}) : super(key: key);
-   String name;
-   String image;
+class HomeTwo extends StatelessWidget {
+  //HomeTwo({Key? key,required this.name, required this.image}) : super(key: key);
+   String iddata;
+  // String image;
 
+HomeTwo({required this.iddata,
 
+  // required this.image
+});
   @override
-  State<HomeTwo> createState() => _HomeTwoState();
-}
 
-class _HomeTwoState extends State<HomeTwo> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: FutureBuilder(
-        future: moonfall(widget.name),
-        builder: (BuildContext context, AsyncSnapshot<MoonfallModel> snapshot) {
-          if(snapshot.hasData){
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Card(
-                    child: Center(child:
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Scaffold(
+          backgroundColor: Colors.black,
+          body: FutureBuilder(
+            future: moonfall(iddata),
+            builder: (BuildContext context,
+                AsyncSnapshot<MoonfallModel> snapshot) {
+              if (snapshot.hasData) {
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    children: [
+                     SizedBox(height: 20,),
 
-               snapshot.data!.budget != null ?     Text(snapshot.data!.budget!.toString()):Container()),
-                  ),
-                ),
-                Image.network(widget.image)
+                  snapshot.data!.title != null ?    Container(
+                    width: 350,
+                    child: Padding (
+                            padding: EdgeInsets.fromLTRB(33, 19, 0, 0),
+                            child:
 
-              ],
-            );
-          }
-          return Container();
+                            Text(snapshot.data!.title.toString(),style: GoogleFonts.bevan(color: Colors.white,fontSize: 20),)),
+                  ): Text("Trending Movie"),
+                       SizedBox(
+                         width: 350,
+                         child: Padding(
+                           padding: const EdgeInsets.fromLTRB(33, 0, 0, 0),
+                           child: Text("RunTime:  " + snapshot.data!.runtime.toString()+ "m",style: TextStyle(color: Colors.white,)),
+                         ),
+                       ),
+                          SizedBox(
+                            width: 350,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(33, 0, 0, 0),
+                              child: Container(child: Text("language:  " + snapshot.data!.originalLanguage.toString(),style: TextStyle(color: Colors.white),)),
+                            ),
+                          ),
 
-        },)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 38.0),
+                        child: SizedBox(
+                          height: 200,
+                          width: 200,
+                          child: Card(
+
+                            child:
+                            Image.network("https://image.tmdb.org/t/p/w500"+snapshot.data!.posterPath.toString(),fit: BoxFit.cover,),
+                          ),
+                        ),
+                      ),
+
+                      Card(
+                        elevation: 19,
+                              color: Colors.blueGrey,
+                              child: Text(snapshot.data!.title .toString(),style: TextStyle(color: Colors.white),),
+                            ),
+
+                      SizedBox(
+                        width: 350,
+                        child: Padding(padding: EdgeInsets.fromLTRB(30, 10, 5, 0),
+                          child: Text(snapshot.data!.overview.toString(),style: TextStyle(color: Colors.white),),
+
+                        ),
+                      ),
+                 Padding(
+                   padding: const EdgeInsets.only(top: 18.0),
+                   child: SizedBox(
+                     height: 30,
+                     width: 350,
+                     child: Padding(
+                       padding: const EdgeInsets.only(left :26.0),
+                       child: Row(
+                         children: [
+                           Icon(Icons.star,color: Colors.yellow,),
+                           Text( snapshot.data!.voteAverage.toString()+"/10",style: TextStyle(color: Colors.white),),
+                           
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+
+
+
+                    ],
+                  ),) ;
+              }
+              return Container();
+            },)
+      ),
     );
   }
 }
+
+
+
